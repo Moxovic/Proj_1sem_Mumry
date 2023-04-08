6f4090ae938472849172b5b7a1dfd2f0fdef3555
andud14 <antondud1@yandex.ru>
2023-04-08 16:26:16 +0300

    Начали заполнять таблицы

--- PZ_15/PZ_15.py
+++ PZ_15/PZ_15.py
@@ -16,9 +16,11 @@ with sq.connect('dekanat.db') as con:
 cur.execute("""CREATE TABLE IF NOT EXISTS kafedri (
  id_kafedri INTEGER PRIMARY KEY AUTOINCREMENT,
  nazvanie_kafedri VARCHAR,
- id_fakulteti INTEGER FOREIGN KEY
+ id_fakulteti INTEGER,
+ FOREIGN KEY (id_fakulteti) REFERENCES fakulteti(id_fakulteti) ON DELETE CASCADE ON UPDATE CASCADE
  )""")
 # Создаем таблицу кафедры
+
 
 with sq.connect('dekanat.db') as con:
     cur = con.cursor()
@@ -26,9 +28,12 @@ with sq.connect('dekanat.db') as con:
 cur.execute("""CREATE TABLE IF NOT EXISTS specialnosti (
  id_specialnost INTEGER PRIMARY KEY AUTOINCREMENT,
  nazvanie_specialnosti VARCHAR,
- id_fakulteti INTEGER FOREIGN KEY
+ id_kafedri INTEGER,
+ FOREIGN KEY (id_kafedri) REFERENCES kafedri(id_kafedri) ON DELETE CASCADE ON UPDATE CASCADE
  )""")
 # Создаём таблицу специальности
+
+
 with sq.connect('dekanat.db') as con:
     cur = con.cursor()
 # cur.execute("DROP TABLE IF EXISTS users")
@@ -37,6 +42,7 @@ cur.execute("""CREATE TABLE IF NOT EXISTS predmeti (
  nazvanie_predmeta VARCHAR
  )""")
 # Создаём таблицу предметы
+
 
 with sq.connect('dekanat.db') as con:
     cur = con.cursor()
@@ -52,13 +58,17 @@ with sq.connect('dekanat.db') as con:
 # cur.execute("DROP TABLE IF EXISTS users")
 cur.execute("""CREATE TABLE IF NOT EXISTS ychebni_plan (
  id_ychebni_plan INTEGER PRIMARY KEY AUTOINCREMENT,
- id_specialnost INTEGER FOREIGN KEY, 
- id_predmeti INTEGER FOREIGN KEY,
- id_forma_sdachi_predmeta INTEGER FOREIGN KEY,
+ id_specialnost INTEGER , 
+ id_predmeti INTEGER ,
+ id_forma_sdachi_predmeta INTEGER ,
  kol_vo_leksionnih_chasov INTEGER,
  kol_vo_prakticheskih_chasov INTEGER,
  kol_vo_laboratornih_chasov INTEGER,
- kursovaya_rabota BOOLEAN
+ kursovaya_rabota BOOLEAN,
+ 
+ FOREIGN KEY (id_specialnost) REFERENCES specialnosti(id_specialnost) ON DELETE CASCADE ON UPDATE CASCADE,
+ FOREIGN KEY (id_predmeti) REFERENCES predmeti(id_predmeti) ON DELETE CASCADE ON UPDATE CASCADE,
+ FOREIGN KEY (id_forma_sdachi_predmeta) REFERENCES forma_sdachi_predmeta(id_forma_sdachi_predmeta) ON DELETE CASCADE ON UPDATE CASCADE
  )""")
 #Создаем таблицу учебный план
 
@@ -68,7 +78,7 @@ with sq.connect('dekanat.db') as con:
 cur.execute("""CREATE TABLE IF NOT EXISTS abiturients (
  id_abiturients INTEGER PRIMARY KEY,
  second_name TEXT,
- name TEXT,
+ name_abiturients TEXT,
  surname TEXT,
  sex TEXT,
  date_born DATETIME,
@@ -76,10 +86,13 @@ cur.execute("""CREATE TABLE IF NOT EXISTS abiturients (
  phone TEXT,
  email TEXT,
  date_postup DATETIME,
- specialnost TEXT
+ id_specialnost TEXT,
+ FOREIGN KEY (id_specialnost) REFERENCES specialnosti(id_specialnost) ON DELETE CASCADE ON UPDATE CASCADE
+ 
  )""")
+#Добавили приставку id к столбцу специальность для связи таблиц в бд 
+# Создаем таблицу абитуриенты
 
-#Создаем таблицу абитуриенты
 
 with sq.connect('dekanat.db') as con:
     cur = con.cursor()
@@ -88,12 +101,15 @@ cur.execute("PRAGMA foreign_keys = ON")
 cur.execute("""CREATE TABLE IF NOT EXISTS ucheb_karta (
  id_karta INTEGER PRIMARY KEY,
  fio_student TEXT,
- group TEXT,
+ groupa TEXT,
  specialnost TEXT,
- predmet TEXT FOREIGN KEY,
- forma_sdachi_predmeta TEXT FOREIGN KEY,
- mark INTEGER
- FOREIGN KEY () REFERENCES TABLICA() ON DELETE CASCADE ON UPDATE CASCADE
+ id_predmeti TEXT ,
+ id_forma_sdachi_predmeta TEXT,
+ mark INTEGER,
+ FOREIGN KEY (id_predmeti) REFERENCES predmeti(id_predmeti) ON DELETE CASCADE ON UPDATE CASCADE,
+ FOREIGN KEY (id_forma_sdachi_predmeta) REFERENCES forma_sdachi_predmeta(id_forma_sdachi_predmeta) ON DELETE CASCADE ON UPDATE CASCADE
  )""")
 
-#Создаем таблицу учебная карточка
+# Создаем таблицу учебная карточка
+#Тоже самое сделали и здесь со столбцами предметы и форма сдачи предмета
+#FOREIGN KEY () REFERENCES TABLICA() ON DELETE CASCADE ON UPDATE CASCADE
